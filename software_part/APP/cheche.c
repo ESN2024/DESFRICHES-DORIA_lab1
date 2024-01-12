93e3fb0f917fdaccd748d5aa3cb021691fe0d9d7
R-one92 <erwan.desfriches.doria@gmail.com>
2024-01-12 08:32:27 +0100

    Premier ajout de l'avancement du TP1

---
+++ software_part/APP/cheche.c
@@ -0,0 +1,7 @@
+#include "system.h"
+#include "alt_types.h"
+#include "altera_avalon_timer_regs.h"
+#include "altera_avalon_timer.h"
+#include "sys/alt_irq.h"
+#include "sys/alt_sys_init.h"
+#include "altera_avalon_pio_regs.h"
