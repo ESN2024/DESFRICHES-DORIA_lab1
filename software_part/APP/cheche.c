9f5977d3ad17df2b2dbf8d76ef43b6bbc3738571
R-one92 <erwan.desfriches.doria@gmail.com>
2024-01-12 10:54:55 +0100

    Chenillard implémenté

--- software_part/APP/cheche.c
+++ software_part/APP/cheche.c
@@ -1,7 +1,23 @@
 #include "system.h"
+#include "sys/alt_stdio.h"
 #include "alt_types.h"
-#include "altera_avalon_timer_regs.h"
-#include "altera_avalon_timer.h"
-#include "sys/alt_irq.h"
-#include "sys/alt_sys_init.h"
+#include "io.h"
+#include <stdlib.h>
+#include <unistd.h>
 #include "altera_avalon_pio_regs.h"
+
+#define DELAI 1000000
+
+int main()
+{
+    //allumage d'une led à la fois
+    while(1)
+    {
+        for (__uint8_t i=0; i<8; i++)
+        {
+            IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, 1 << i);
+            usleep(DELAI);
+        }
+    }
+    
+}
